381ca94ec1cdaf967595809a216c28062876aa1c
Francis M. Tyers <ftyers@prompsit.com>
2012-07-06 19:58:43 +0000

    update modes file and add some ar->mt stuff

--- dev/testvoc.sh
+++ dev/testvoc.sh
@@ -2,8 +2,8 @@
 
 cd $(dirname $0)
 
-#echo "==Arabic->Maltese===========================";
-#bash inconsistency.sh ar-mt > /tmp/ar-mt.testvoc; bash inconsistency-summary.sh /tmp/ar-mt.testvoc ar-mt
-#echo ""
+echo "==Arabic->Maltese===========================";
+bash inconsistency.sh ar-mt > /tmp/ar-mt.testvoc; bash inconsistency-summary.sh /tmp/ar-mt.testvoc ar-mt
+echo ""
 echo "==Maltese->Arabic===========================";
 bash inconsistency.sh mt-ar > /tmp/mt-ar.testvoc; bash inconsistency-summary.sh /tmp/mt-ar.testvoc mt-ar
